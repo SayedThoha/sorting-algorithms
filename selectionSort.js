aae318ad6cacc8421bec08e5f942cf9d77715e08
SayedThoha <147703696+SayedThoha@users.noreply.github.com>
2025-07-28 15:51:29 +0530

    selection sort added

---
+++ selectionSort.js
@@ -0,0 +1,21 @@
+let arr = [11, -2, 3, 4, 5 - 11, 4, 1, 0];
+
+function selectionSort(arr) {
+  let len = arr.length;
+
+  for (let i = 0; i < len - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+      if (minIndex !== i) {
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+      }
+    }
+  
+  return arr;
+}
+
+console.log(selectionSort(arr));

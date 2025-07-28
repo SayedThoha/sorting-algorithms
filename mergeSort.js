6eaedd2b7148abd46a24b0ad959ae8701e57fa7d
SayedThoha <147703696+SayedThoha@users.noreply.github.com>
2025-07-28 22:10:27 +0530

    merge sort added

---
+++ mergeSort.js
@@ -0,0 +1,37 @@
+let arr = [11, -2, 3, 4, 5, -11, 4, 1, 0];
+
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  let sortedArr = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+
+  return [...sortedArr, ...left, ...right];
+}
+
+console.log(mergeSort(arr));
+
+/* 
+time complexity :
+best :o(n log n)
+avg: o(n log n)
+worst: o(n log n)
+
+space complexity:
+o(n)
+*/
